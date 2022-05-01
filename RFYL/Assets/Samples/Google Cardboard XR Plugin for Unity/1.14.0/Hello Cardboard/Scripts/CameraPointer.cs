bd8d96ef8ed80db5cac88fc53e9f416372d1eff5
btothey99 <btothey99@ewhain.net>
2022-05-02 03:13:11 +0900

    220502

---
+++ RFYL/Assets/Samples/Google Cardboard XR Plugin for Unity/1.14.0/Hello Cardboard/Scripts/CameraPointer.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9fd97c1546138e7b2982c0661d760153fa5aecadbf211bc5c973a9dc55810d1a
+size 2214
---
+++ RFYL/Assets/Samples/Google Cardboard XR Plugin for Unity/1.14.0/Hello Cardboard/Scripts/VrModeController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a0aad93103da224f0b6491564a7700d82724d93a5c163aba29c8e400f2fa1159
+size 5390
